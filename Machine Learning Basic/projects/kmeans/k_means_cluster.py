e703067fea169ffaaefecc41b1db27ab970ad238
niranjanviladkar <nviladkar@gmail.com>
2018-03-25 17:46:13 +0530

    Mini projects done. Started with unsupervised customer segmentation project.

--- Machine Learning Basic/projects/kmeans/k_means_cluster.py
+++ Machine Learning Basic/projects/kmeans/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -42,40 +42,52 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 ### load in the dict of dicts containing all the data on each person in the dataset
 #data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 data_dict = pickle.load( open("./final_project_dataset.pkl", "rb") )
-print(data_dict.keys())
-# print(pd.DataFrame(data_dict).describe().transpose())
-#print(min(data_dict["exercised_stock_options"]))
-#print(min(pd.DataFrame(data_dict)["exercised_stock_options"]))
-#print(max(pd.DataFrame(data_dict)["exercised_stock_options"]))
 
-### there's an outlier--remove it! 
+variable = "exercised_stock_options"
+l = [val[variable] \
+     for val in data_dict.values() \
+     if isinstance(val[variable], int)\
+    ]
+
+print(min(l), max(l))
+
+variable = "salary"
+l = [val[variable] \
+     for val in data_dict.values() \
+     if val[variable] != "NaN"\
+    ]
+
+print(min(l), max(l))
+
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+#features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-#for f1, f2, _ in finance_features:
-#    plt.scatter( f1, f2 )
-#plt.show()
+for f1, f2 in finance_features:
+    plt.scatter( f1, f2 )
+plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-#kmeans = KMeans(n_clusters=2, random_state=123456).fit(finance_features)
-#pred = kmeans.labels_
+kmeans = KMeans(n_clusters=2, random_state=123456).fit(finance_features)
+pred = kmeans.labels_
 
 
 
